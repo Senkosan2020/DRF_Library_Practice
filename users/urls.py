332ae426fdb81210657a4c0d5a7e4d0ea3961d7f
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-10-15 12:06:21 +0200

    Add JWT obtain/refresh endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import RegisterView, MeView
 
 urlpatterns = [
     path("", RegisterView.as_view(), name="users-register"),
     path("me/", MeView.as_view(), name="users-me"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
