2c95e581e5982ef15e0f0429307c307d1f9f00d4
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-10-15 12:02:58 +0200

    Add users-me endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, MeView
 
 urlpatterns = [
     path("", RegisterView.as_view(), name="users-register"),
+    path("me/", MeView.as_view(), name="users-me"),
 ]
