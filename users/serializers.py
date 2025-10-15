d6af5b11c9be84f41bacf51ea0d9d6777c965623
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-10-15 11:46:50 +0200

    Add MeSerializer in users

--- users/serializers.py
+++ users/serializers.py
@@ -16,3 +16,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class MeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "is_staff")
+        read_only_fields = ("email", "is_staff")
