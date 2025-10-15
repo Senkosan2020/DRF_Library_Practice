f6ae44eb28ed10c8654ac6cafd9199eedd5b8c66
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-10-15 11:50:17 +0200

    Add MeView in users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, MeSerializer
 
 User = get_user_model()
 
@@ -8,3 +8,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = MeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
