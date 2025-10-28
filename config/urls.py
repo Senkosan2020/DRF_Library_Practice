3a16082c821fb301a18de6e723d81d7493474621
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-10-28 19:17:07 +0100

    Add books routes at /api/books/

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("users/", include("users.urls")),
+    path("api/", include("books.urls")),
 ]
