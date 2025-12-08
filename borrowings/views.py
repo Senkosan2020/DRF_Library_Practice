25165c4a69500c5700a21d7b3e9f1c62d2b2fa25
Ukrtelecom <zabegins777@gmail.com>
2025-12-08 14:04:37 +0100

    Add BorrowingViewSet in borrowings/views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, mixins
+from .models import Borrowing
+from .serializers import BorrowingReadSerializer, BorrowingCreateSerializer
 
-# Create your views here.
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ["get", "post"]  # забороняємо put/patch/delete
+
+    def get_queryset(self):
+        qs = Borrowing.objects.select_related("book", "user").order_by("-id")
+        return qs if self.request.user.is_staff else qs.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        return BorrowingCreateSerializer if self.action == "create" else BorrowingReadSerializer
