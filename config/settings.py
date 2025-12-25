9e0267634f4e999744529c816144cf2f2450a21a
Ukrtelecom <zabegins777@gmail.com>
2025-12-25 08:37:59 +0100

    Enable search by title and author

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_THROTTLE_RATES": {
         "borrowing_burst": "5/min",
