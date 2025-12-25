27ad43e8e3ffda5e6f93d48608cd139f310d6116
Ukrtelecom <zabegins777@gmail.com>
2025-12-25 02:18:34 +0100

    Add register Book/Borrowing with useful list columns

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Borrowing
 
-# Register your models here.
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "user", "book",
+        "borrow_date", "expected_return_date", "actual_return_date", "is_active",
+    )
+    list_filter = ("actual_return_date",)
+    search_fields = ("user__email", "book__title")
+    ordering = ("-borrow_date",)
