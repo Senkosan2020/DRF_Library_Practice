7573cceb5bda8c8d3a2faadd3965abacf3a01dca
Ukrtelecom <zabegins777@gmail.com>
2025-12-25 09:10:36 +0100

    Add available_only filter to return only in-stock titles

--- books/filters.py
+++ books/filters.py
@@ -6,6 +6,14 @@ class BookFilter(django_filters.FilterSet):
     author = django_filters.CharFilter(field_name="author", lookup_expr="icontains")
     cover = django_filters.CharFilter(field_name="cover")  # exact HARD/SOFT
 
+    # NEW: return only books with inventory > 0 when true
+    available_only = django_filters.BooleanFilter(method="filter_available_only")
+
     class Meta:
         model = Book
-        fields = ("title", "author", "cover")
+        fields = ("title", "author", "cover", "available_only")
+
+    def filter_available_only(self, queryset, name, value):
+        if value:
+            return queryset.filter(inventory__gt=0)
+        return queryset
